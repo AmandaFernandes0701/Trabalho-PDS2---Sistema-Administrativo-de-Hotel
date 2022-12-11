befcd4c3ee36d2ed036d5bc7f289d55a5dd3c2fc
S2AmandaS2 <118071794+S2AmandaS2@users.noreply.github.com>
2022-12-11 17:12:31 -0300

    Delete hotel.cpp

--- src/hotel.cpp
+++
@@ -1,7 +0,0 @@
-#include "Hotel.hpp"
-
-Hotel::Hotel();
-    void Hotel::insertHospede(Hospede *_hospede);
-    void Hotel::insertHospede(Funcionario *_Funcionario);
-    void Hotel::insertHospede(Quarto *_Quarto);
-    void Hotel::insertHospede(Reserva *_Reserva);
