c0c23cf61b5f9b315711ffd69784fcc65f6d38a4
S2AmandaS2 <118071794+S2AmandaS2@users.noreply.github.com>
2022-12-15 22:08:39 -0300

    Update Hospede.cpp

--- src/entidades/Hospede.cpp
+++ src/entidades/Hospede.cpp
@@ -1,26 +1,36 @@
 #include "Hospede.hpp"
 
-bool Hospede::login(string email, string senha){
-    // TODO
-    // Conecta ao banco de dados e verifica login
+Hospede::Hospede(std::string nome, std::string email, std::string senha, std::string cpf, std::string telefone){
+    this->nome = nome;
+    this->email = email;
+    this->senha = senha;
+    this->cpf = cpf;
+    this->numero_telefone = telefone;
+}
+
+Hospede::Hospede(std::string email, std::string senha){
+    this->email = email;
+    this->senha = senha;
+}
+
+bool Hospede::login(BancoDeDados* banco_de_dados){
+    bool retorno = banco_de_dados->login(this->email, this->senha);
+    if(retorno == true)
+        std::cout<<"Login realizado com sucesso!"<<std::endl;
+    else{
+        std::cout<<"Falha no login\nVerifique se digitou ";
+        std::cout<<"o email e senha corretamente."<<std::endl;
+    }
+    return retorno;
 }
 
 bool Hospede::cadastrar(BancoDeDados* banco_de_dados){
-    // TODO: Ler nome completo sem erro
-    // TODO: Verificar se dados foram preenchidos corretamente
-    string nome, email, senha, cpf, telefone;
-    cout<<"Digite todos os dados de cadastro"<<endl;
-    cin>>nome; // não le espaço
-    cin>>email;
-    cin>>senha;
-    cin>>cpf;
-    cin>>telefone;
 
-    bool retorno = banco_de_dados->cadastrar_hospede(nome, email,
-                    senha, cpf, telefone);
+    bool retorno = banco_de_dados->cadastrar_hospede(this->nome, this->email,
+                    this->senha, this->cpf, this->numero_telefone);
     if(retorno == true)
-        cout<<"Cadastro realizado com sucesso"<<endl;
+        std::cout<<"Cadastro realizado com sucesso"<<std::endl;
     else
-        cout<<"Cadastro não realizado"<<endl;
+        std::cout<<"Cadastro não realizado"<<std::endl;
     return false;
 }
