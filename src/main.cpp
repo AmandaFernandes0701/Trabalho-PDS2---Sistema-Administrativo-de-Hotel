00b94a7761dbf29aabe2dd41505f9b7bb222d79d
S2AmandaS2 <118071794+S2AmandaS2@users.noreply.github.com>
2022-12-15 22:09:26 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 #include "BancoDeDados.hpp"
 #include <iostream>
 
-using namespace std;
 
 int main()
 {
@@ -10,17 +9,46 @@ int main()
     int escolha;
 
     while(true){
-        cout<<"Digite:"<<endl;
-        cout<<"1 para cadastrar hóspede"<<endl;
-        cout<<"2 para logar hóspede"<<endl;
-        cin>>escolha;
+        std::cout<<"Digite:"<<std::endl;
+        std::cout<<"1 para cadastrar hóspede"<<std::endl;
+        std::cout<<"2 para logar hóspede"<<std::endl;
+        std::cout<<"3 para sair"<<std::endl;
+        std::cin>>escolha;
 
         if(escolha == 1)
         {
-            Hospede *hospede = new Hospede();
+            std::string nome, email, senha, cpf, telefone;
+
+            std::cin.ignore(); // https://pt.stackoverflow.com/a/319643
+            std::cout<<"Digite o seu nome: ";
+            std::getline(std::cin, nome);
+            std::cout<<"Digite o seu email: ";
+            std::cin>>email;
+            std::cout<<"Digite uma senha: ";
+            std::cin>>senha;
+            std::cout<<"Digite o seu CPF: ";
+            std::cin>>cpf;
+            std::cout<<"Digite o seu telefone: ";
+            std::cin>>telefone;
+
+            Hospede *hospede = new Hospede(nome, email, senha, cpf, telefone);
+            // TODO: Verificar se dados foram preenchidos corretamente
+            // hospede->verificar_corretude_dados()
             hospede->cadastrar(&banco_de_dados);
             break;
-        }else if(escolha == 2){}
+        }else if(escolha == 2){
+            std::string email, senha;
+            std::cout<<"Digite o seu email: ";
+            std::cin>>email;
+            std::cout<<"Digite a sua senha: ";
+            std::cin>>senha;
+            Hospede *hospede = new Hospede(email, senha);
+            bool retorno = hospede->login(&banco_de_dados);
+
+        }
+        else if(escolha == 3){
+            break;
+        }
     }
 
     return 0;
