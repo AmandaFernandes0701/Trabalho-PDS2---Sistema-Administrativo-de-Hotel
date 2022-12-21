b6af5dd88b5a66bb85cb40f21021af4aeec0f84c
S2AmandaS2 <118071794+S2AmandaS2@users.noreply.github.com>
2022-12-21 19:10:48 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include "Hospede.hpp"
 #include "BancoDeDados.hpp"
 #include <iostream>
 
@@ -7,47 +6,102 @@ int main()
 {
     BancoDeDados banco_de_dados = BancoDeDados();
     int escolha;
+    bool FLAG = true;
 
-    while(true){
+    while(FLAG == true){
         std::cout<<"Digite:"<<std::endl;
         std::cout<<"1 para cadastrar hóspede"<<std::endl;
         std::cout<<"2 para logar hóspede"<<std::endl;
-        std::cout<<"3 para sair"<<std::endl;
+        std::cout<<"3 para logar funcionário"<<std::endl;
+        std::cout<<"4 para reservar quarto"<<std::endl;
+        std::cout<<"5 para sair"<<std::endl;
         std::cin>>escolha;
 
-        if(escolha == 1)
+        switch (escolha)
         {
-            std::string nome, email, senha, cpf, telefone;
+            case 1:{
+                std::string nome, email, senha, cpf, telefone;
 
-            std::cin.ignore(); // https://pt.stackoverflow.com/a/319643
-            std::cout<<"Digite o seu nome: ";
-            std::getline(std::cin, nome);
-            std::cout<<"Digite o seu email: ";
-            std::cin>>email;
-            std::cout<<"Digite uma senha: ";
-            std::cin>>senha;
-            std::cout<<"Digite o seu CPF: ";
-            std::cin>>cpf;
-            std::cout<<"Digite o seu telefone: ";
-            std::cin>>telefone;
+                std::cin.ignore(); // https://pt.stackoverflow.com/a/319643
+                std::cout<<"Digite o seu nome: ";
+                std::getline(std::cin, nome);
+                std::cout<<"Digite o seu email: ";
+                std::cin>>email;
+                std::cout<<"Digite uma senha: ";
+                std::cin>>senha;
+                std::cout<<"Digite o seu CPF: ";
+                std::cin>>cpf;
+                std::cout<<"Digite o seu telefone: ";
+                std::cin>>telefone;
 
-            Hospede *hospede = new Hospede(nome, email, senha, cpf, telefone);
-            // TODO: Verificar se dados foram preenchidos corretamente
-            // hospede->verificar_corretude_dados()
-            hospede->cadastrar(&banco_de_dados);
-            break;
-        }else if(escolha == 2){
-            std::string email, senha;
-            std::cout<<"Digite o seu email: ";
-            std::cin>>email;
-            std::cout<<"Digite a sua senha: ";
-            std::cin>>senha;
-            Hospede *hospede = new Hospede(email, senha);
-            bool retorno = hospede->login(&banco_de_dados);
+                // TODO: Verificar se dados foram preenchidos corretamente
+                bool retorno = banco_de_dados.cadastrar_hospede(nome, email, senha, cpf, telefone);
+                if(retorno == false)
+                    std::cout<<"Cadastro nao realizado"<<std::endl;
+                else
+                    std::cout<<"Cadastro realizado com sucesso"<<std::endl;
 
-        }
-        else if(escolha == 3){
-            break;
+                break;
+            }
+            case 2:{
+
+                std::string email, senha;
+
+                std::cout<<"Digite o seu email: ";
+                std::cin>>email;
+                std::cout<<"Digite a sua senha: ";
+                std::cin>>senha;
+
+                bool retorno = banco_de_dados.login_hospede(email, senha);
+                if(retorno == false){
+                    std::cout<<"Falha no login\nVerifique se digitou ";
+                    std::cout<<"o email e senha corretamente."<<std::endl;
+                }else
+                    std::cout<<"Login correto"<<std::endl;
+
+                break;
+            }
+            case 3:{
+
+                std::string email, senha;
+
+                std::cout<<"Digite o seu email: ";
+                std::cin>>email;
+                std::cout<<"Digite uma senha: ";
+                std::cin>>senha;
+
+                bool retorno = banco_de_dados.login_funcionario(email, senha);
+                if(retorno == false){
+                    std::cout<<"Falha no login\nVerifique se digitou ";
+                    std::cout<<"o email e senha corretamente."<<std::endl;
+                }else
+                    std::cout<<"Login correto"<<std::endl;
+
+                break;
+            }
+            case 4:{
+
+                // std::string data;
+                // std::cout<<"Digite a data (DD/MM/YYYY) para ver os quartos disponíveis: ";
+                // std::cin>>data;
+                // std::cout<<"Verificando os quartos disponíveis nessa data..."<<std::endl;
+                // std::vector<std::string> quartos = hospede->verificar_quartos_disponiveis(&banco_de_dados, data);
+                // if(quartos.size() == 0){
+                //     std::cout<<"Não há quartos livres nessa data"<<std::endl;
+                // }else{
+                //     // Imprima os quartos disponíveis
+                //     // Pergunte ao usuário qual quarto ele quer
+                //     reservar_quarto(numero_quarto, data);
+                // }
+
+                break;
+            }
+            case 5:{
+                FLAG = false;
+                break;
+            }
+            default:
+                break;
         }
     }
 
