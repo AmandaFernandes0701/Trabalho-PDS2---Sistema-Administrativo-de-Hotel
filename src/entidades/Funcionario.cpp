7983f2ed1409584a67de87f8740dce37dd58b116
S2AmandaS2 <118071794+S2AmandaS2@users.noreply.github.com>
2022-12-21 19:08:21 -0300

    Update Funcionario.cpp

--- src/entidades/Funcionario.cpp
+++ src/entidades/Funcionario.cpp
@@ -1,15 +1,31 @@
 #include "Funcionario.hpp"
 
-
-Funcionario::Funcionario(string email, string senha){
-    this->email = email;
-    this->senha = senha;
+Funcionario::Funcionario(){
+    this->email = "funcionario@hotel.com";
+    this->senha = "pds2";
 }
 
-bool Funcionario::login(string email, string senha) {
-    if(this->email != email || this->senha != senha)
+bool Funcionario::verificar_email(std::string email){
+    if(this->email == email)
+        return true;
+    else
         return false;
-
-    return true;
 }
 
+bool Funcionario::verificar_senha(std::string senha){
+    if(this->senha == senha)
+        return true;
+    else
+        return false;
+}
+
+// TODO
+// Implementar USER STORY seguinte
+// --> Acesso somente a histórico de reservas do hóspede
+
+/*
+Como funcionário do hotel, eu desejo acessar determinadas informações dos
+hóspedes cadastrados na base de dados do meu sistema, assim, posso tomar
+decisões de como adequar o hotel para atender ainda melhor ao perfil de hóspede
+que nele se hospeda.
+*/
